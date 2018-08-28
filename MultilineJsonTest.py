de547b0810a0c5c58f332ecb416bc8593950aa23
Yury Bakhmutski <YURY_BAKHMUTSKI@epam.com>
2018-08-28 14:46:52 +0300

    Json Multiline Test

--- MultilineJsonTest.py
+++ MultilineJsonTest.py
@@ -1,19 +1,11 @@
-from pyspark import SparkContext, SparkConf
-spark_conf = SparkConf().setAppName("multiline json")
-spark_context = SparkContext(conf=spark_conf)
-
-#DATABRICKS_JSON_PATH = "src/test/resources/databricks_json_example.json"
-DATABRICKS_JSON_PATH = "test.py"
-#address = "/path/to/the/log/on/hdfs/*.gz"
-
-def print_funct(l):
-    print(l)
-    return l
-
+"""MultilineJsonTest.py"""
+from __future__ import print_function
 from pyspark.sql import SparkSession
-spark = SparkSession.builder.appName("SimpleApp").getOrCreate()
-jsonRDD = spark.read.text(DATABRICKS_JSON_PATH).cache()
-#jsonRDD = spark_context.textFile(DATABRICKS_JSON_PATH)
-#jsonRDD.collect().foreach(println)
 
-jsonRDD.map(lambda l: print_funct(l)).collect()
+logFile = "resources/databricks_json_example.json"  # Should be some file on your system
+spark = SparkSession.builder.appName("Multiline Json Test").getOrCreate()
+logData = spark.read.text(logFile).cache()
+
+logData.foreach(print)
+
+spark.stop()
